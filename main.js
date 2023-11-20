9eb06e9e600e939acd0253b64eb5b104c27d1ec1
farhia-siyaad <148630711+farhia-siyaad@users.noreply.github.com>
2023-11-20 13:58:06 +0300

    Assignment 3

---
+++ main.js
@@ -0,0 +1,14 @@
+let number=2
+while (number<=20) {
+     document.write(number ,"    " );
+     number= number+2
+}
+
+
+
+document.write("<br>")
+let oddnum=1
+while(oddnum<=19){
+     document.write(oddnum ,"  ")
+     oddnum= oddnum+2
+}
